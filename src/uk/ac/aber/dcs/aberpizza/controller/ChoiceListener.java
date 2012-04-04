a224f0fdf36429a7b08c19dfe02ba44ae652c5fa
Samuel Sherar <s.sherar@live.com>
2012-04-04 04:46:28 +0100

    Added listener to the buttons

---
+++ src/uk/ac/aber/dcs/aberpizza/controller/ChoiceListener.java
@@ -0,0 +1,22 @@
+package uk.ac.aber.dcs.aberpizza.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import uk.ac.aber.dcs.aberpizza.data.Product;
+import uk.ac.aber.dcs.aberpizza.gui.Choices;
+
+public class ChoiceListener implements ActionListener {
+	Choices c;
+	
+	public ChoiceListener(Choices choices) {
+		c = choices;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.print(arg0.getActionCommand()+"\t");
+		System.out.println(c.find(arg0.getActionCommand()));
+	}
+
+}
--- src/uk/ac/aber/dcs/aberpizza/gui/Choices.java
+++ src/uk/ac/aber/dcs/aberpizza/gui/Choices.java
@@ -1,6 +1,7 @@
 package uk.ac.aber.dcs.aberpizza.gui;
 import javax.swing.*;
 
+import uk.ac.aber.dcs.aberpizza.controller.ChoiceListener;
 import uk.ac.aber.dcs.aberpizza.data.Product;
 
 import java.awt.*;
@@ -9,16 +10,32 @@ import java.util.ArrayList;
 
 
 public class Choices extends JPanel {
+	
 	private ArrayList<Product> c;
+	private ChoiceListener l;
+	
 	public Choices() {
 		super(new GridLayout(0,4));
+		l = new ChoiceListener(this);
 	}
 	
 	public void init(ArrayList<Product> choices) {
-		for(Product p : choices) {
-			this.add(new JButton(p.getName()));
+		c = choices;
+		for(Product p : c) {
+			JButton tmp = new JButton(p.getName());
+			tmp.addActionListener(l);
+			this.add(tmp);
 		}
 		this.doLayout();
+	}
+	
+	public Product find(String name) {
+		for(Product p : c) {
+			if(p.getName() == name) {
+				return p;
+			}
+		}
+		return null;
 	}
 }
 
