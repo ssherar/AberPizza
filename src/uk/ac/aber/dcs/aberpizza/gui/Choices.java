9a4a8f9390b0fd4efbeef968504125b8f24989f6
Samuel Sherar <s.sherar@live.com>
2012-04-04 03:48:24 +0100

    MOAR BUTTONZ

--- src/uk/ac/aber/dcs/aberpizza/gui/Choices.java
+++ src/uk/ac/aber/dcs/aberpizza/gui/Choices.java
@@ -7,8 +7,8 @@ import java.awt.event.*;
 
 public class Choices extends JPanel {
 	public Choices() {
-		super(new GridLayout(0,3));
-		for(int i = 0; i < 21; i++) {
+		super(new GridLayout(0,4));
+		for(int i = 0; i < 40; i++) {
 			this.add(new JButton(""+i));
 		}
 	}
