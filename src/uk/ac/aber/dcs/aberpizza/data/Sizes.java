860a701e7eb5c052ee6ac12c786f529e819aabad
Sam Sherar <s.sherar@live.com>
2012-04-12 18:19:39 +0100

    Size enum now has an overriden toString to make it loooooook beautiful

--- src/uk/ac/aber/dcs/aberpizza/data/Sizes.java
+++ src/uk/ac/aber/dcs/aberpizza/data/Sizes.java
@@ -7,4 +7,17 @@ public enum Sizes {
 	CAN,
 	SMALL_BOTTLE,
 	LARGE_BOTTLE;
+	
+	@Override
+	public String toString() {
+		String[] s = super.toString().split("_");
+		String ret = "";
+		for(int i = 0; i < s.length; i++) {
+			s[i] = s[i].substring(0, 1) + s[i].substring(1, s[i].length()).toLowerCase();
+			ret = s[i] + " ";
+		}
+		ret = ret.substring(0, ret.length() - 1);
+		return ret;
+	}
+	
 }
